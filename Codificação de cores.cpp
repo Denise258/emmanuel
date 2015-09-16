fe1a50296179dd3aa0d3ace6b241f28fe7a8aa64
Denise258 <deniselins@outlook.com>
2015-09-16 13:27:14 -0300

    Update Codificação de cores.cpp

--- Codificação de cores.cpp
+++ Codificação de cores.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
-#include <cmath>
-int main(){
-	int bits, qtdpontos, coresdisponiveis, tmbits;
-	float qtdbytes;
-	cout <<"Digite a quantidade de bits"<<endl;
-	cin>>bits;
-	cout <<"Digite a quantidade de pontos"<<endl;
-	cin>>qtdpontos;
-	coresdisponiveis = pow(2,bits);
-	tmbits = bits*qtdpontos;
-	qtdbytes =  tmbits/8;
-	cout <<" A quantidade de cores disponiveis são:"<<coresdisponiveis<<endl;
-	cout <<" O tamanho da imagem em bits é:"<<tmbits<<endl;
-	cout <<" O tamanho da imagem em bytes é:"<<qtdbytes<<endl;
+
+int main() {
+setlocale (LC_ALL, "Portuguese");
+	int ponto, qtdlinha, qtdcoluna, bitslinhas, bitscolunas, totalbits, totalbytes;
+	ponto = 3;
+	cout <<"Digite a quantidade de linhas da figura"<<endl;
+	cin>>qtdlinha;
+	cout <<"Digite a quantidade de colunas da figura"<<endl;
+	cin>>qtdcoluna;
+	bitslinhas = qtdcoluna * ponto;
+	bitscolunas =  qtdlinha * ponto;
+	totalbits = bitslinhas * bitscolunas;
+	totalbytes = totalbits*8;
+	cout <<"A quantidade de bits de cada linha é:"<<bitslinhas<<endl;
+	cout <<"A quantidade de bits de cada coluna é:"<<bitscolunas<<endl;
+	cout <<"A quantidade de bits da figura é:"<<totalbits<<endl;
+	cout <<"A Quantidade de bytes da figura é:"<<totalbytes<<endl;
 	
+
 	return 0;
 }
