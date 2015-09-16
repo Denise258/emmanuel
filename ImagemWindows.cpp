406dc064002df640c4944ba8d19f7b825af2f9e7
Denise258 <deniselins@outlook.com>
2015-09-16 14:59:54 -0300

    Create ImagemWindows.cpp

---
+++ ImagemWindows.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+setlocale(LC_ALL, "Portuguese");
+	int qtdlinhas, qtdcolunas, bits, qtdpontos,bytes, mega, giga; 
+	cout <<"Digite a quantidade de linhas da imagem"<<endl;
+	cin>>qtdlinhas;
+	cout <<"Digite a quantidade de colunas da imagem"<<endl;
+	cin>>qtdcolunas;
+	cout <<"Digite a profundidade de cor da imagem"<<endl;
+	cin>>bits;
+	qtdpontos = qtdlinhas * qtdcolunas;
+	bytes = qtdcolunas * bits;
+	mega = bytes * 1024;
+	giga = mega * 1024;
+	
+	cout <<"A quantidade de pontos da figura é:"<<qtdpontos<<endl;
+	cout <<"O tamanho da figura em bytes é:"<<bytes<<endl;
+	cout <<"O tamanho da figura em mega é:"<<mega<<endl;
+	cout <<"O tamanho da figura em Giga é:"<<giga<<endl;
+	
+	return 0;
+}
