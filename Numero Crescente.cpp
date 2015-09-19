0ffd7939028cc8921eea4f673a1adf3d31ebb798
Denise258 <deniselins@outlook.com>
2015-09-18 23:01:05 -0300

    Create Numero Crescente.cpp

---
+++ Numero Crescente.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int x,y,z;
+	cout<<"Digite o primeiro numero"<<endl;
+	cin>>x;
+	cout<<"Digite o segundo numero "<<endl;
+	cin>>y;
+	cout<<"Digite o terceiro numero "<<endl;
+	cin>>z;
+		if (x==y || x==z || x==z){
+			cout<<"Numero Iguais o programa será encerrado"<<endl;
+
+		}
+		else
+			if (x<y && x<z){
+				if (y<z){
+					cout<<"Os números na ordem crescente : "<<x<<y<<z<<endl;
+				}
+				else {
+					cout<<"Os números na ordem crescente : "<<x<<z<<y<<endl;
+				}
+			}
+			if (y<x && y<z)			
