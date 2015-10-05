f66f29c8668f16f07d93d3c640a5e8154cf71961
Denise258 <deniselins@outlook.com>
2015-10-05 19:41:24 -0300

    Create positivenumber.cpp

---
+++ positivenumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Write a positve number"<<endl;
+	cin>>num;
+	if (num < 0){
+		cout<<"Invalid Number· The program will be closed."<<endl;
+			}
+			else {
+			if (num % 2 ==0 || num % 3 == 0 || num % 5 == 0){
+				cout<<" Number is divisible for 2, 3 and 5"<<endl;
+						}
+						else {
+									cout<<"The number is not divisible for 2, 3 and 5"<<endl;
+									}
+						}
+return 0;
+}
+	
